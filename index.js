e78266ea08a204bd34df95bee6f20b4a54c7e7f5
pipopotamasu <ec0156hx@gmail.com>
2018-01-23 20:03:58 +0900

    remove colon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
 
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken();
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
-export default axios;
+export default axios
