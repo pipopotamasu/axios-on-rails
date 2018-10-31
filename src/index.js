9ce61b03b1b76db10443fd8b536e1a6be0314de5
Daniel Moniz <daniel.moniz@gmail.com>
2018-10-31 16:50:30 -0400

    Add X-Requested-With header for request.xhr? .

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
 
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+axios.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest"
 
 export default axios
